c05122657c952f82119dce739c0538bbdb19f3eb
Snoworange420 <n401571@gymnasiumnovum.nl>
2023-02-21 22:53:32 +0100

    Initial commit v4: HSB fix

--- src/main/java/nl/snoworange/cranberry/misc/java/skillissue/Good.java
+++
@@ -1,9 +0,0 @@
-package nl.snoworange.cranberry.misc.java.skillissue;
-
-import nl.snoworange.cranberry.Main;
-
-public class Good {
-    public void info(String message) {
-        Main.LOGGER.info(message);
-    }
-}
